e2a30cf5b1161521c15d7e9d273f106615223ef8
masihterjaga <40725678+masihterjaga@users.noreply.github.com>
2018-09-20 22:49:58 +0700

    Create coba.js

---
+++ coba.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    function redirect() {
+        window.location.assign('https://masihterjaga.blogspot.com/');
+    }
+
+    function check() {
+        if ($('#massugeng').length === 0) {
+            redirect();
+        } 
+else if ($("#massugeng").attr("style") === "visibility:hidden") {
+            redirect();
+        }
+else if ($("#massugeng").attr("style") === "visibility:hidden;") {
+            redirect();
+        }
+else if ($("#massugeng").attr("style") === "visibility: hidden") {
+            redirect();
+        }
+else if ($("#massugeng").attr("style") === "visibility: hidden;") {
+            redirect();
+        }
+else if ($('#vimagz').length === 0) {
+            redirect();
+        }
+        if ($('div').length === 0) {
+            redirect();
+        } else if ($('#vimagz').length === 0) {
+            redirect();
+        } else if ($("#vimagz").attr("href") !== "https://masihterjaga.blogspot.com") {
+            redirect();
+        } 
+else if ($("#vimagz").attr("title") !== "MASIH TERJAGA") {
+            redirect();
+        }
+ else if ($("#vimagz").attr("style") === "visibility:hidden") {
+            redirect();
+        }
+else if ($("#vimagz").attr("style") === "visibility:hidden;") {
+            redirect();
+        }
+else if ($("#vimagz").attr("style") === "visibility: hidden") {
+            redirect();
+        }
+else if ($("#vimagz").attr("style") === "visibility: hidden;") {
+            redirect();
+        }
+else if ($("#vimagz").attr("rel") === "nofollow") {
+            redirect();
+        }
+else if ($('#vimagz').text() !== "Masih Terjaga") {
+            redirect();
+        }
+    }
+    check();
+    setInterval(function() {
+        check()
+    }, 2000);
+});
